26e489bc6567c54b13d26e897747dc36949cc02a
yveseve <wannarot.c@gmail.com>
2026-03-26 22:44:40 +0700

    index

--- nav.js
+++ nav.js
@@ -1,64 +1,25 @@
-function addNavTool(){
-    var div = document.getElementById("navtool");
-    div.innerHTML=`
-    <div class="container-fluid bg-nav sticky-top p-0">
-            <nav class="navbar navbar-expand-lg navbar-light p-0" id="menu">
-                <a href="index.html" class="navbar-brand bg-primary py-4 px-5 me-0">
-                    <h1 class="mb-0"><i class="bi bi-cookie"></i>FRIDGE AND OVEN</h1>
-                </a>
-                <button
-                    type="button"
-                    class="navbar-toggler me-4"
-                    data-bs-toggle="collapse"
-                    data-bs-target="#navbarCollapse">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse p-3" id="navbarCollapse">
-                    <div class="navbar-nav mx-auto">
-                        
-                            <a href="index.html" class="nav-item nav-link active">Home</a>
-                            <a href="about.html" class="nav-item nav-link">About</a>
-                            <a href="how-to-order.html" class="nav-item nav-link">How to Order</a>
-                                <div class="nav-item dropdown">
-                                    
-                                    <label for="menu-toggle" class="nav-link">Menu ▾</label>
-                                    <input type="checkbox" id="menu-toggle" />
-
-                                    <ul class="submenu">
-                                        <li>
-                                            <label for="nested-toggle" class="dropdown-item nav-link" style="font-size: small">Whole cake ▾</label>
-                                            <input type="checkbox" id="nested-toggle" />
-
-                                            <ul class="nested-submenu nav-item">
-                                                <li><a href="our-cakes-edited.html" class="dropdown-item nav-link" style="font-size:x-small">all cakes</a>
-                                                </li>
-                                                <li><a href="categories.html" class="dropdown-item nav-link" style="font-size: x-small">by categories</a></li>
-                                                <li><a href="tart-n-pie.html" class="dropdown-item nav-link" style="font-size: x-small">tart and pie</a></li>
-                                            </ul>
-                                        </li>
-                                        <li><a href="sliced-cakes.html" class="dropdown-item nav-link" style="font-size: small">sliced cakes</a></li>
-                                        <li>
-                                            <label for="nested-toggle2" class="dropdown-item nav-link" style="font-size: small">cookies ▾</label>
-                                            <input type="checkbox" id="nested-toggle2" />
-
-                                            <ul class="nested-submenu2">
-                                                <li><a href="cookies.html" class="dropdown-item nav-link" style="font-size:x-small">soft baked</a></li>
-                                                <li><a href="sables.html" class="dropdown-item nav-link" style="font-size: x-small">sables</a></li>
-                                            </ul>
-                                        </li>
-                                    </ul>
-                                </div>
-                            
-
-                                
-                        <a href="workshop.html" class="nav-item nav-link active">Workshops</a>
-                        <a href="contact.html" class="nav-item nav-link">Contact</a>
-                    </div>
-                </div>
-            </nav>
+function addNavTool() {
+    const navHTML = `
+<nav class="top-brand-bar">
+        <div class="container">
+            <a href="index.html" class="nav-brand-logo">FRIDGE AND OVEN</a>
+            <div class="nav-links">
+                <a href="index.html">HOME</a>
+                <a href="our-cakes-edited.html">CAKESS</a>
+                <a href="workshop.html">WORKSHOPS</a>
+            </div>
         </div>
+    </nav>
     `;
-   
     
+    const navElement = document.getElementById('navtool');
+    if (navElement) {
+        navElement.innerHTML = navHTML;
+        
+        // ตรวจสอบหน้าปัจจุบันเพื่อเติม class 'active'
+        const currentPage = window.location.pathname;
+        if (currentPage.includes("instructors.html")) {
+            document.getElementById("nav-instructors").classList.add("active");
+        }
+    }
 }
